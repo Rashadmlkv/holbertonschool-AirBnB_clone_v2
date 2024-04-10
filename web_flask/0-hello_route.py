1056cd8109e60348f4233182f4ea54c89ff95b85
Rashadmlkv <resadmlkv@gmail.com>
2024-04-10 00:58:52 +0000

    ayrilsak oluruz biz

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 from flask import Flask
 """Helllo """
+
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def helloHBNB():
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
